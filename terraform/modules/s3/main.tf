27af7ee0b5abbf93866ba98f0fac7fc4d0f3d902
Rajeev Ranjan <rajeevranjan456@gmail.com>
2026-07-08 00:23:23 +0530

    Added server side encryption on S3 bucket

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -59,3 +59,22 @@ resource "aws_s3_bucket_versioning" "portfolio" {
   }
 
 }
+###############################################################
+# Server Side Encryption
+###############################################################
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "portfolio" {
+
+  bucket = aws_s3_bucket.portfolio_bucket.id
+
+  rule {
+
+    apply_server_side_encryption_by_default {
+
+      sse_algorithm = "AES256"
+
+    }
+
+  }
+
+}
